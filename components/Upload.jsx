8c4ed28519e5d6a38a3b9b27b85265a59600ec18
itxadii <diamondgamerz49@gmail.com>
2025-10-30 17:14:14 +0530

    Changed Upload text

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -128,7 +128,7 @@ const Upload = () => {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M7 16a4 4 0 01-.88-7.9A5 5 0 0115.9 6h1.1a5 5 0 01.9 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                     </svg>
                     <p className="text-gray-400 font-medium">Click to upload</p>
-                    <p className="text-sm text-gray-500">mp3 (max 5MB)</p>
+                    <p className="text-sm text-gray-500">MP3 only (max 50MB)</p>
                     
                     {selectedFile && <p className="mt-3 text-gray-300  text-center">{selectedFile.name}</p>}
                     {uploadProgress > 0 && uploadProgress < 100 && (
